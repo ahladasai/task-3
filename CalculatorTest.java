34cd66bfce26188a3a88514eab941b9720355599
ahladasai <51901002+ahladasai@users.noreply.github.com>
2020-03-16 09:03:29 +0530

    Add files via upload

---
+++ CalculatorTest.java
@@ -0,0 +1,28 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void testAdd() {
+		Calculator c=new Calculator();
+		assertEquals(9,c.add(5,4));
+	}
+
+	@Test
+	public void testSub() {
+		Calculator c=new Calculator();
+		assertEquals(1,c.sub(5, 4));
+	}
+	@Test
+	public void testMul() {
+		Calculator c=new Calculator();
+		assertEquals(20,c.mul(5,4));
+	}
+	@Test
+	public void testDiv() {
+		Calculator c=new Calculator();
+		assertEquals(0,c.div(5, 4));
+	}
+}
